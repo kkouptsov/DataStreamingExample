f51f3dc303560f0ac2ec8db090b76e54681f69cd
Konstantin Kouptsov <kkouptsov@wolfram.com>
2022-07-16 13:55:38 -0400

    Add FileGenerator class

---
+++ filegenerator.py
@@ -0,0 +1,11 @@
+class FileGenerator:
+
+    # Default parameters
+    TARGET_DIRECTORY = 'in'
+    FILE_COUNT = 100
+    FILE_MIN_SIZE = 1024         # 1 KiB
+    FILE_MAX_SIZE = 1024*1024    # 1 MiB
+    TIME_DURATION = 600*10**3    # 10 min
+    TIME_MIN = 1                 # 1 ms
+    TIME_MAX = 10**3             # 1 s
+
