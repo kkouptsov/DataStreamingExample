c3090782651be6066c86c64c9a51071a3b7526c9
Konstantin Kouptsov <kkouptsov@wolfram.com>
2022-07-16 14:01:22 -0400

    Add getNextTime, makeFileSize, makeFileName

--- filegenerator.py
+++ filegenerator.py
@@ -1,4 +1,6 @@
 import time, math, os
+import numpy as np
+
 def time2ms(time):
     """ Helper function to convert time.time() to milliseconds
     """
@@ -13,6 +15,15 @@ def getArg(kw, name, cast, default):
     if isinstance(res, cast):
         return res
     return default
+
+def randomFromRange(min, max):
+    """ Get a random point from the interval with descreasing probability.
+    Using a suitable but arbitrary chosen distribution. This could be improved. 
+    """
+    res = min + np.random.exponential((max - min)/5)
+    while res > max:
+        res = min + np.random.exponential((max - min)/5)
+    return res
 
 
 class FileGenerator:
@@ -32,4 +43,23 @@ class FileGenerator:
         self.next_time = self.start_time
         self.count = self.FILE_COUNT
         self.outdir = os.path.abspath(getArg(kw, 'input_dir', str, self.TARGET_DIRECTORY))
+
+    def getNextTime(self):
+        """ Calculate a random time interval between successive file creation.
+        Given that min and max times differ by a few orders of magnitude, it makes
+        sense to generate time intervals using a non-uniform distribution
+        """
+        now = time2ms(time.time())
+        if now > self.next_time:
+            return randomFromRange(self.TIME_MIN, math.min(self.TIME_MAX, self.end_time - now))
+        
+    def makeFileSize(self):
+        """ Calculate a random file size
+        """
+        return math.floor(randomFromRange(self.FILE_MIN_SIZE, self.FILE_MAX_SIZE))
+
+    def makeFileName(self):
+        """ Generate a random file name
+        """
+        return os.path.join(self.outdir, str(time2ms(time.time())))
 
